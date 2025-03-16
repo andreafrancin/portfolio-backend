1696c7eb0c3c9f1b07ab90a62ec3579d4540825a
andreafrancin <andfrancin@gmail.com>
2025-03-16 15:13:28 +0100

    Added redirection to /admin from /

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home_redirect
 
 urlpatterns = [
+  path('', home_redirect, name='home_redirect'),
     path('admin/', admin.site.urls),
     path('api/', include('projects.urls')),
 ]
