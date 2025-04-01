8d50c784ab45fdc613ca8c9b3a091ace7b0878d7
Canada <canada@Guillems-MacBook-Pro.local>
2025-04-01 21:50:21 +0200

    Added key field to the admin list display

--- translations/admin.py
+++ translations/admin.py
@@ -4,5 +4,5 @@ from translations.models import Translation
 # Register your models here.
 @admin.register(Translation)
 class TranslationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'text_en', 'text_es', 'text_ca')
-    search_fields = ('id', 'text_en', 'text_es', 'text_ca')
+    list_display = ('id', 'key', 'text_en', 'text_es', 'text_ca')
+    search_fields = ('id', 'key', 'text_en', 'text_es', 'text_ca')
